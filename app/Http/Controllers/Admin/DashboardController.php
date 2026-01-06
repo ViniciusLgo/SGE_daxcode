218c3d49d6f0a3b7da5e0a525d7be3d895b64c7e
Vinicius Lgo <vinicruzlago@gmail.com>
2026-01-06 17:03:03 -0300

    feat: melhorias no dashboard admin

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Aluno;
 use App\Models\Disciplina;
+use App\Models\Despesa;
 use App\Models\Professor;
 use App\Models\Turma;
+use App\Models\User;
 use App\Models\Setting;
 use Illuminate\Support\Facades\DB;
 
@@ -65,6 +67,15 @@ class DashboardController extends Controller
 
         /*
         |--------------------------------------------------------------------------
+        | USUARIOS RECENTES
+        |--------------------------------------------------------------------------
+        */
+        $recentUsuarios = User::orderByDesc('id')
+            ->limit(5)
+            ->get();
+
+        /*
+        |--------------------------------------------------------------------------
         | ALUNOS RECENTES
         |--------------------------------------------------------------------------
         */
@@ -101,6 +112,33 @@ class DashboardController extends Controller
             ->whereDay('data_nascimento', now()->day)
             ->count();
 
+        $proximosAniversarios = Aluno::with('user')
+            ->whereNotNull('data_nascimento')
+            ->orderByRaw(
+                "STR_TO_DATE(CONCAT(YEAR(CURDATE()) + " .
+                "(DATE_FORMAT(data_nascimento, '%m-%d') < DATE_FORMAT(CURDATE(), '%m-%d'))," .
+                " '-', DATE_FORMAT(data_nascimento, '%m-%d')), '%Y-%m-%d')"
+            )
+            ->limit(6)
+            ->get();
+
+        /*
+        |--------------------------------------------------------------------------
+        | FINANCEIRO RESUMO
+        |--------------------------------------------------------------------------
+        */
+        $totalDespesasMes = Despesa::whereYear('data', now()->year)
+            ->whereMonth('data', now()->month)
+            ->sum('valor');
+
+        $totalDespesasAno = Despesa::whereYear('data', now()->year)
+            ->sum('valor');
+
+        $despesasRecentes = Despesa::with('categoria')
+            ->orderByDesc('data')
+            ->limit(5)
+            ->get();
+
         /*
         |--------------------------------------------------------------------------
         | VIEW
@@ -115,11 +153,16 @@ class DashboardController extends Controller
             'alunosPorTurma',
             'professoresPorDisciplina',
             'evolucaoMatriculas',
+            'recentUsuarios',
             'recentAlunos',
             'ocupacaoMedia',
             'turmasSemProfessor',
             'alunosSemDocumentos',
-            'aniversariantesHoje'
+            'aniversariantesHoje',
+            'proximosAniversarios',
+            'totalDespesasMes',
+            'totalDespesasAno',
+            'despesasRecentes'
         ));
     }
 }
